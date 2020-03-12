1880bd2603e1ed31bfcb14fee4b1b39844137419
brian <riceb53@gmail.com>
2020-03-11 22:09:25 -0400

    add happy sad paths for contacts create and update

--- app/controllers/api/contacts_controller.rb
+++ app/controllers/api/contacts_controller.rb
@@ -25,22 +25,26 @@ class Api::ContactsController < ApplicationController
       latitude: latitude,
       longitude: longitude
     )
-    @contact.save 
-    render 'show.json.jb'
+    if @contact.save 
+      render 'show.json.jb'
+    else
+      render json: {errors: @contact.errors.full_messages}
+    end
   end
 
   def update
     @contact = Contact.find(params[:id])
-    @contact.update(
-      first_name: params[:first_name],
-      last_name: params[:last_name],
-      email: params[:email],
-      phone_number: params[:phone_number],
-      bio: params[:bio],
-      middle_name: params[:middle_name]
-    )
-    @contact.save 
-    render 'show.json.jb'
+    @contact.first_name = params[:first_name] || @contact.first_name
+    @contact.last_name = params[:last_name] || @contact.last_name
+    @contact.email = params[:email] || @contact.email
+    @contact.phone_number = params[:phone_number] || @contact.phone_number
+    @contact.bio = params[:bio] || @contact.bio
+    @contact.middle_name = params[:middle_name] || @contact.middle_name
+    if @contact.save
+      render 'show.json.jb'
+    else
+      render json: {errors: @contact.errors.full_messages}
+    end
   end
 
   def destroy
